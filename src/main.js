01a6e240c00a22fce3e1c98865332b2efc4576f8
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-10-03 19:37:47 +0700

    '.'

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+const app = express()
+try {
+  // connect db atlas
+  ;(async () => {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DATABASE}:${process.env.PASSWORD}@cluster0.xbd5gx2.mongodb.net/app_mobile`
+    )
+    console.log('connected db ')
+  })()
+} catch (error) {
+  console.log('ket noi that bai')
+}
+app.listen(process.env.PORT, () => {
+  console.log(`connected port ${process.env.PORT}`)
+})
