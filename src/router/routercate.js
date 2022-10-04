2ba01051bda0151ca8b024f22ba75ced351faf13
= <huynguyenz2002@gmail.com>
2022-10-04 16:35:33 +0700

    delete Category

--- src/router/routercate.js
+++ src/router/routercate.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import { addCategory } from '../controllers/category/addCate'
+import { deleteCategory } from '../controllers/category/deleteCate'
 import { getCategory } from '../controllers/category/findCate'
 
 const router = Router()
 
 router.get('/listCategory',getCategory)
 router.post('/addCategory',addCategory)
+router.delete('/deleteCategory/:id',deleteCategory)
 
 export default router
