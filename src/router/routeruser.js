ded616a1d6dad2dc3ecbd65295b870d314704a30
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-10-06 12:20:27 +0700

    đăng nhập tài khoản chủ nhà

--- src/router/routeruser.js
+++ src/router/routeruser.js
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import loginUser from '../controllers/user/signin'
+import { loginUser, loginHost } from '../controllers/user/signin'
 import { createHost, createUser } from '../controllers/user/signup'
 const router = Router()
 router.route('/signup').post(createUser)
 router.route('/signin').post(loginUser)
 router.route('/host/signup').post(createHost)
-router.route('/host/signin').post(createHost)
+router.route('/host/signin').post(loginHost)
 export default router
