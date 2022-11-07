ea4bc30cf3594412283817eb51b94439e94684b7
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-11-07 13:52:18 +0700

    order history and user infor

--- src/router/routerorder.js
+++ src/router/routerorder.js
@@ -2,12 +2,15 @@ import { Router } from 'express'
 import {
   createOrder,
   ListOrder,
+  orderNotices,
   orderNotSeem,
   updateStatus,
+  updateStatusDone,
 } from '../controllers/order/order'
 const router = Router()
 router.route('/addorder').post(createOrder)
-router.route('/order').post(ListOrder)
+router.route('/order').post(ListOrder).patch(updateStatus)
 router.route('/oderseem').post(orderNotSeem)
-router.route('/order/:id').patch(updateStatus)
+router.route('/orderdone').patch(updateStatusDone)
+router.route('/ordernotices').post(orderNotices)
 export default router
